d5970acfae80a57230b063993aa8828d98ebf6c2
pvcapuano <pvcapuano@gmail.com>
2022-09-15 19:32:43 -0300

    ex array

---
+++ javascript/ex-array-js.js
@@ -0,0 +1,27 @@
+hitchedSpaceships = [
+  ["Fenix", 8, true],
+  ["Golias", 10, true],
+  ["Helmet", 5, false],
+  ["Elemental", 3, true],
+  ["Darwin", 15, false],
+];
+
+let filtrado = hitchedSpaceships
+  .filter((spaceship) => {
+    return spaceship[1] > 9;
+  })
+  .map((spaceship) => {
+    return spaceship[0];
+  });
+
+let pendente = hitchedSpaceships.findIndex((spaceship) => {
+  return spaceship[2] == false;
+});
+
+let maiusculo = hitchedSpaceships.map((spaceship) => {
+  return spaceship[0].toUpperCase();
+});
+
+console.log(filtrado);
+console.log(pendente);
+console.log(maiusculo);
