9ec0473d5579c5bc623bd5807b9a2ec41375513c
pvcapuano <pvcapuano@gmail.com>
2022-09-27 19:43:25 -0300

    add new class

--- javascript/modulos/src/index.js
+++ javascript/modulos/src/index.js
@@ -1,6 +1,7 @@
 const Planet = require("./planet");
 const solarSystem = require("./solar_system");
 const moment = require("moment");
+import armamentKind from "./armaments";
 import Spaceship from "./spaceship";
 
 const earth = new Planet("Terra", 500);
@@ -12,3 +13,6 @@ solarSystem.planets.push(new Planet("Marte", 800));
 console.log(solarSystem);
 
 console.log(moment().format("h:m"));
+
+const spaceship = new Spaceship("USS", "PV", armamentKind.laser);
+console.log(spaceship);
